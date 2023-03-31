0d61ade526238320d36866e2b8fd1b2bc1060685
Javier Vasquez <vp.javier25@gmail.com>
2023-03-31 08:55:29 -0500

    fix(auth flow): save token in localStorage

---
+++ client/src/Pages/ResetPAssword/ResetPassword.jsx
@@ -0,0 +1,63 @@
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Button,
+    Input,
+    Container,
+    VStack,
+    Box,
+    Flex,
+    Spacer,
+    Heading,
+    Divider,
+    Center,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
+import { forgotPasswordSchema } from "./errors";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+
+export default function ResetPassword (){
+
+    const {token} = useParams(); 
+    console.log(token)
+    const [result, setResult] = useState("");
+
+    const authHeader = { Authorization: `Bearer ${token}` };
+
+    const submit = async (data) => {
+        try {
+           const res = await axios.put("/reset", {headers: authHeader}); 
+           setResult(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+        
+
+    }
+
+    return (
+        <Container mt= "100px">
+            <Heading>Log in</Heading>
+            <form onSubmit={handleSubmit(submit)}>
+                <VStack spacing="24px">
+
+                    <FormControl isInvalid={errors.user_password ? true : false}>
+                        <FormLabel>Enter the new password</FormLabel>
+                        <Input type='text' placeholder="Enter your password" {...register('user_password')} />
+                        {!errors.user_password ? null : <FormErrorMessage>{errors.user_password?.message}</FormErrorMessage>}
+                    </FormControl>
+                    
+                    <SignUp/>
+                </VStack>
+
+            </form>
+            {result? <span>{result}</span>: null}
+        </Container>
+    )
+}
