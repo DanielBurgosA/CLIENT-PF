d2dc7123dee164f22a8114adc8df5181372cd5da
AlanSilcher <silcheralan@gmail.com>
2023-04-01 07:50:22 -0300

    cambios a main git push

--- src/Pages/Pagos/Pagos.jsx
+++ src/Pages/Pagos/Pagos.jsx
@@ -1,31 +1,63 @@
 import style from "./Pagos.module.css";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
 import { cleanLink, linkPaymentPlatform } from "../../Redux/Slicers/paymentSlicer";
-import {Button} from "@chakra-ui/react"
+import {
+  FormControl,
+  FormLabel,
+  Button,
+  Input,
+  Flex,
+} from "@chakra-ui/react";
 
 export default function Pagos() {
   const dispatch = useDispatch();
   const payLink = useSelector((state) => state.paymentLink.payLink);
+  const LogInStatus = useSelector (state => state.login.status)
+  console.log("esto es log in status", LogInStatus)
+
+  /* const state = useSelector (state => state)
+  console.log("esto es el state", state) */
+
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
 
   useEffect(()=>{
-    payLink&&window.open(payLink, "_blank");
+    payLink && window.open(payLink, "_blank");
     return ()=>{
       dispatch(cleanLink())
     }
   },[payLink, dispatch])
 
-  const handleButtonClick = () => {
-      dispatch(linkPaymentPlatform());
-    };
+  /* const handleButtonClick = () => {
+    
+    
+    }; */
+
+    const Submit = (data) => {
+      const form = {
+        userName: "Alan",
+        project: "Proyecto de prueba",
+        amount: data.donation
+      }
+      dispatch(linkPaymentPlatform(form));
+      console.log("esto es data",data)
+      console.log("esto es form", form)
+  }
 
   return (
-    <div className={style.Prov}>
-      <div className={style.column}>
-        <Button onClick={handleButtonClick} className={style.button}>
-          Realizar donacion con PayPal
-        </Button>
-      </div>
+    <div>
+      <form onSubmit={handleSubmit(Submit)}>
+                    <FormControl>
+                        <FormLabel>Amount to donate</FormLabel>
+                        <Input type="number" placeholder="enter the amount you want to donate (in dollars)" {...register('donation')} />
+                    </FormControl>
+
+                    <Flex>
+                    <Button type="submit" className={style.button}>Realizar donacion con PayPal</Button>
+                    </Flex>
+            </form>
     </div>
   );
 }
