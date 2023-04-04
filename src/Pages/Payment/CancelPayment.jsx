ade0f735af242a4d3e238fe0261581d75b1d3497
AlanSilcher <silcheralan@gmail.com>
2023-04-03 21:31:13 -0300

    Cambio de estilos

---
+++ src/Pages/Payment/CancelPayment.jsx
@@ -0,0 +1,17 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function ExecutePayment() {
+  return (
+    <Box
+      bg="#ff6b6b"
+      p={6}
+      borderRadius="md"
+      boxShadow="md"
+      textAlign="center"
+    >
+      <Heading as="h1" size="lg" color="white">
+        Payment was successful
+      </Heading>
+    </Box>
+  );
+}
