03c8a179f85a694aa978e5a86672c6396f04af65
Alejandro Centeno <alejocm21@gmail.com>
2023-04-04 09:59:05 -0400

    modificacion de detalle del projecto

--- src/Pages/DetailProject/DetailProject.jsx
+++ src/Pages/DetailProject/DetailProject.jsx
@@ -8,14 +8,17 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   Box,
+  Container,
+  Stack,
   Text,
-  Button,
   Image,
-  Center,
+  Flex,
   VStack,
+  Button,
   Heading,
-  Card,
-  Spinner
+  SimpleGrid,
+  StackDivider,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import style from "./DetailProject.module.css";
 import AddComment from "../../Components/CreateComment/CreateComment.jsx";
@@ -35,68 +38,82 @@ export default function DetailProject() {
   }, [dispatch, id]);
 
   let projectById = useSelector((state) => state.project.projectId);
+  console.log('projectById :>> ', projectById);
 
   return (
-    <Box maxH="1.5">
-      {projectById && Object.keys(projectById) ? (
-        <Box textAlign="justify">
-          <VStack spacing={3}>
-            <Heading as="b" fontSize="4rem" margin="1rem 3rem">
+    <Box>
+    <Container maxW={'7xl'}>
+      <SimpleGrid
+        columns={{ base: 1, lg: 2 }}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 18, md: 24 }}>
+        <Flex>
+          <Image
+            rounded={'md'}
+            alt={'product image'}
+            src={
+              projectById.image
+            }
+            fit={'cover'}
+            align={'center'}
+            w={'100%'}
+            h={{ base: '100%', sm: '400px', lg: '500px' }}
+          />
+        </Flex>
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Box as={'header'}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
               {projectById.name}
             </Heading>
-            <Card
-              variant="outline"
-              borderColor="#D8DEE4"
-              maxW="70rem"
-              className={style.Prov}
-            >
-              <Text fontSize="20px" padding="7px 10px 0px">
-                Creador: ramdom user{" "}
-              </Text>
-              <Text fontSize="15px" padding="5px">
-                {projectById.location}
-              </Text>
-            </Card>
-            <Center>
-              <Image
-                boxSize="400px"
-                objectFit="cover"
-                borderRadius="full"
-                align="center"
-                src={projectById.image}
-                alt={projectById.name}
+            <Text
+              color={useColorModeValue('gray.900', 'gray.400')}
+              fontWeight={300}
+              fontSize={'2xl'}>
+            ${projectById.currentAmount} / ${projectById.cost}
+            </Text>
+          </Box>
+
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={'column'}
+            divider={
+              <StackDivider
+                borderColor={useColorModeValue('gray.200', 'gray.600')}
               />
-            </Center>
-            <Card
-              bg="#F6F8FA"
-              variant="outline"
-              borderColor="#D8DEE4"
-              maxW="70rem"
-            >
-              <Text fontSize="20px" padding="15px">
+            }>
+            <VStack spacing={{ base: 4, sm: 6 }}>
+              <Text
+                color={useColorModeValue('gray.500', 'gray.400')}
+                fontSize={'2xl'}
+                fontWeight={'300'}>
+                Descripción
+              </Text>
+              <Text fontSize={'lg'}>
                 {projectById.description}
               </Text>
-            </Card>
-          </VStack>
-        </Box>
-      ) : (
-        <Center height="25rem">
-              <Spinner
-                thickness="4px"
-                speed="0.65s"
-                emptyColor="gray.200"
-                color="blue.500"
-                size="xl"
-              />
-            </Center>
-      )}
-      <Box>
-        <Link to="/projects">
-          <Button margin="1rem 3rem" _hover={{ background: "#C9EEFF" }}>
-            Back
+            </VStack>
+          </Stack>
+          <Button
+            rounded={'none'}
+            w={'full'}
+            mt={8}
+            size={'lg'}
+            py={'7'}
+            bg={useColorModeValue('gray.900', 'gray.50')}
+            color={useColorModeValue('white', 'gray.900')}
+            textTransform={'uppercase'}
+            _hover={{
+              transform: 'translateY(2px)',
+              boxShadow: 'lg',
+            }}>
+            Add to cart
           </Button>
-        </Link>
-      </Box>
+        </Stack>
+      </SimpleGrid>
+    </Container>
       {LogInStatus ? <AddComment /> : ""}
       <div>
         <CommentsProjectById />
